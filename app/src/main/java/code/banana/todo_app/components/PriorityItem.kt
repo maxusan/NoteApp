982da771f23664dc3c9a655637fcc6d2cc72518d
Maksym Kovalchuk <maksym.kovalchuk.natife@gmail.com>
2024-01-18 10:43:41 +0200

    fix to priority item

--- app/src/main/java/code/banana/todo_app/components/PriorityItem.kt
+++ app/src/main/java/code/banana/todo_app/components/PriorityItem.kt
@@ -1,9 +1,12 @@
 package code.banana.todo_app.components
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -21,9 +24,11 @@ import code.banana.todo_app.util.colorByPriority
 @Composable
 fun PriorityItem(priority: Priority, modifier: Modifier = Modifier) {
     Row(verticalAlignment = Alignment.CenterVertically, modifier = modifier) {
-        Canvas(modifier = Modifier.size(PRIORITY_INDICATOR_SIZE)) {
-            drawCircle(color = priority.colorByPriority())
-        }
+        Box(
+            modifier = Modifier
+                .size(PRIORITY_INDICATOR_SIZE)
+                .background(priority.colorByPriority(), shape = CircleShape)
+        )
         Text(
             modifier = Modifier.padding(start = MEDIUM_PADDING),
             text = priority.name,
